c859e5d4a6b6c36c715d62a3f736e3ffbc1aa6ce
Nang_Ying_Lao_Hsaing <6531503162@lamduan.mfu.ac.th>
2025-09-11 14:05:24 +0700

    Updated Room Page including changed to tailwind css

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/postcss"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: { "/api": "http://localhost:5000" } // backend dev server
   }
