f09e2abf4d384470970aa4dc8143c48258d062a0
Nang_Ying_Lao_Hsaing <6531503162@lamduan.mfu.ac.th>
2025-09-11 15:33:05 +0700

    Room changing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/postcss"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    proxy: { "/api": "http://localhost:5000" } // backend dev server
-  }
+    proxy: { '/api': 'http://localhost:5000' },
+  },
 });
+
