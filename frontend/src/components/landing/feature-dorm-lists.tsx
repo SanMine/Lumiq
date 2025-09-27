3a763609095eddb1912a7b863bf8197a0b6d2c38
Swam Yi Phyo <swanphyo444@gmail.com>
2025-09-28 01:06:15 +0700

    chore: featured dorm list section completed

---
+++ frontend/src/components/landing/feature-dorm-lists.tsx
@@ -0,0 +1,32 @@
+import { dorms } from "@/lib/constants";
+import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import { Link } from "react-router";
+import { AiOutlineArrowRight } from 'react-icons/ai';
+
+export default function FeatureDormLists() {
+  return (
+    <section className="py-24 w-full max-w-6xl mx-auto px-8">
+      <div className="max-w-3xl mx-auto flex flex-col items-center">
+        <h2 className="text-3xl font-bold">Featured Dorm Listings</h2>
+        <p className="text-sm text-muted-foreground tracking-wide">Handpicked residence for an amazing student life.</p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-8">
+        {
+          dorms.map(dorm => (
+            <Card key={dorm.id} className="rounded-md flex flex-col p-0 border-none cursor-pointer hover:shadow-lg transition-shadow duration-300">
+              <CardHeader className="p-0 rounded-t-md pb-4">
+                <img src={dorm.image} alt={dorm.name} className="h-52 w-full object-cover rounded-t-md" />
+                <CardTitle className="text-md font-bold px-4 pt-2">{dorm.name}</CardTitle>
+                <CardDescription className="px-4">{dorm.description}</CardDescription>
+              </CardHeader>
+            </Card>
+          ))
+        }
+      </div>
+      <Link to="/dorms" aria-label="See All Dorms" className='flex items-center gap-3 justify-center font-medium'>
+        <AiOutlineArrowRight className='animate-point' size="1.2rem" aria-hidden="true" />
+        See All Dorms
+      </Link>
+    </section>
+  )
+}
